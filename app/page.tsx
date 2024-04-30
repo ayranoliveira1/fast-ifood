7e91851703bbec2d98880b4eefac14c2fe98c1a7
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-04-30 11:23:07 -0300

    Feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Header from "./components/header";
+import Search from "./components/search";
 
 const Home = () => {
   return (
     <>
       <Header />
+
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </>
   );
 };
