d6233445ccfb536f675f46cb4a75d4779d2c60af
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-04-30 15:01:26 -0300

    Feat: add product list and product item componets

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import CategotyList from "./components/categoty-list";
 import Header from "./components/header";
 import Search from "./components/search";
+import ProductList from "./components/product-list";
+import { Button } from "./components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 const Home = () => {
   return (
     <>
       <Header />
 
-      <div className="px-5 pt-6">
+      <div className="px-5 pt-6 font-semibold">
         <Search />
       </div>
 
@@ -26,6 +29,20 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={18} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
