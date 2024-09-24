584c6fbc4cc7d8f2a1364bbca99afa42a1386e69
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-24 11:49:02 -0300

    feat: add a function to highlight the button corresponding to the corrent route when the user is on its page

--- app/components/side-menu.tsx
+++ app/components/side-menu.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Heart, Home, LogIn, ScrollText } from "lucide-react";
 
 const SideMenu = () => {
+  const [isStart, setIsStart] = useState<boolean>(false);
+  const [isOrders, setIsOrders] = useState<boolean>(false);
+  const [isFavorites, setIsFavorites] = useState<boolean>(false);
+
+  function handlepath() {
+    if (window.location.pathname === "/") {
+      return setIsStart(true);
+    }
+
+    if (window.location.pathname === "/orders") {
+      return setIsOrders(true);
+    }
+
+    if (window.location.pathname === "/favorites") {
+      return setIsFavorites(true);
+    }
+
+    setIsStart(false);
+    setIsOrders(false);
+    setIsFavorites(false);
+  }
+
+  useEffect(() => {
+    handlepath();
+  }, []);
+
   return (
     <div className="flex flex-col">
       <h1 className="mt-[-10px] font-semibold">Menu</h1>
@@ -18,7 +47,7 @@ const SideMenu = () => {
       <div className="mt-6 space-y-3">
         <Button
           variant="ghost"
-          className="w-full justify-start gap-3 rounded-3xl"
+          className={`w-full justify-start gap-3 rounded-3xl ${isStart ? "bg-primary text-white" : ""}`}
         >
           <Home size="16" />
           Início
@@ -26,7 +55,7 @@ const SideMenu = () => {
 
         <Button
           variant="ghost"
-          className="w-full justify-start gap-3 rounded-3xl"
+          className={`w-full justify-start gap-3 rounded-3xl ${isOrders ? "bg-primary text-white" : ""}`}
         >
           <ScrollText size="16" />
           Meus Pedidos
@@ -34,7 +63,7 @@ const SideMenu = () => {
 
         <Button
           variant="ghost"
-          className="w-full justify-start gap-3 rounded-3xl"
+          className={`w-full justify-start gap-3 rounded-3xl ${isFavorites ? "bg-primary text-white" : ""}`}
         >
           <Heart size="16" />
           Restaurantes Favoritos
