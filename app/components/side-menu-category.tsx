a848e4435f307bfc71db84f1cb65e71ad622ea00
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-25 11:03:55 -0300

    feat: add Menu Category component

---
+++ app/components/side-menu-category.tsx
@@ -0,0 +1,19 @@
+import { Category } from "@prisma/client";
+import Link from "next/link";
+
+interface MenuCategoryProps {
+  category: Pick<Category, "name" | "id">;
+}
+
+const MenuCategory = ({ category }: MenuCategoryProps) => {
+  return (
+    <Link
+      href={`/categories/${category.id}`}
+      className="w-full rounded-3xl px-4 py-2 hover:bg-primary hover:text-white"
+    >
+      <span className="text-sm font-semibold">{category.name}</span>
+    </Link>
+  );
+};
+
+export default MenuCategory;
