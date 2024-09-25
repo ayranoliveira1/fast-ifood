09be60669d4a691d173ae8ac81d195b4b2aa1359
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-25 11:04:35 -0300

    feat: add Menu List component

---
+++ app/components/side-menu-list.tsx
@@ -0,0 +1,16 @@
+import { db } from "../lib/prisma";
+import MenuCategory from "./side-menu-category";
+
+const MenuList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <div className="flex flex-col gap-4">
+      {categories.map((category) => (
+        <MenuCategory key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default MenuList;
