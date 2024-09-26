84e87f893ebf55f09cd12aadc7a3e21bec4a79fc
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-26 10:34:17 -0300

    feat: add initial Orders page

---
+++ app/orders/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+
+const OrdersPage = () => {
+  return (
+    <>
+      <Header />
+
+      <div>Orders</div>
+    </>
+  );
+};
+
+export default OrdersPage;
