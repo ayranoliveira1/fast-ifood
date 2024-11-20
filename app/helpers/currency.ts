f8a8bf8d7691aa0ea796b38129c9cb035dfdbef7
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-11-19 21:36:54 -0300

    feat: add a comment explaining what the function does

--- app/helpers/currency.ts
+++ app/helpers/currency.ts
@@ -1,3 +1,10 @@
+/**
+ * Formata um valor numérico como uma string de moeda no formato brasileiro (BRL).
+ *
+ * @param value - O valor numérico a ser formatado.
+ * @returns Uma string representando o valor formatado como moeda brasileira.
+ */
+//
 export const formatCurrency = (value: number) => {
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
