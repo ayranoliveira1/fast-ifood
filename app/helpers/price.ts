826ea9ec5ec00ecf44f3176dad669d1b41049baf
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-15 18:15:00 -0300

    feat: add format currency

--- app/helpers/price.ts
+++ app/helpers/price.ts
@@ -11,6 +11,12 @@ export const calculateProduct = (product: Product): number => {
   return Number(product.price) - discount;
 };
 
+/**
+ * Formata um valor numérico como uma string de moeda no formato brasileiro (BRL).
+ *
+ * @param value - O valor numérico a ser formatado.
+ * @returns Uma string representando o valor formatado como moeda brasileira.
+ */
 export const formatCurrency = (value: number): string => {
   return Intl.NumberFormat("pt-BR", {
     style: "currency",
